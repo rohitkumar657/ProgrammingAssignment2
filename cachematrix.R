67168088138361a03f6bdfbf051c1f1900ebb953
rohitkumar657 <rohitkumar657@users.noreply.github.com>
2015-07-27 00:52:05 +0530

    Final Solution Cache Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -12,21 +12,32 @@
 # 2. get the value of the matrix
 # 3. set the value of inverse of the matrix
 # 4. get the value of inverse of the matrix
-makeCacheMatrix <- function(x = matrix()) {
-         inv <- NULL
-        set <- function(y) {
-                x <<- y
-                inv <<- NULL
-                }
-                
-    get <- function() x
-    
-    setinverse <- function(inverse) inv <<- inverse
-    
-    getinverse <- function() inv
-    
-    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
-    
+makeCacheMatrix <- function( x = numeric())
+{    		
+	lov1 = length(x)
+	bob <<- matrix(x,nrow = sqrt(lov1), ncol = sqrt(lov1))
+
+	set_matrix = function(z)
+	{
+	x <<- z
+         	lov2 = length(x)	
+         	bob <<- matrix(x,nrow = sqrt(lov2), ncol = sqrt(lov2))
+         	lee <<- NULL
+         }
+	get_matrix = function()
+	{
+         	bob
+         }
+	set_inverse = function(y)
+	{
+         	lov3 = length(y)
+         	lee <<- matrix(y, nrow = sqrt(lov3), ncol = sqrt(lov3))
+         	}	
+	get_inverse = function()		
+	{
+	lee
+	}		
+	list( set_matrix = set_matrix, get_matrix = get_matrix, set_inverse = set_inverse, get_inverse = get_inverse)
 }
 
 
@@ -35,18 +46,18 @@ makeCacheMatrix <- function(x = matrix()) {
 # computation. Otherwise, it computes the inverse, sets the value in the cache via
 # setinverse function.
 # Assuming the matrix is always invertible.
-cacheSolve <- function(x, ...) {
-        inv <- x$getinverse()
-        
-        if(!is.null(inv)) {
-        
-        message("getting cached data.")
-        
-        return(inv)
-        
-    }
-   data <- x$get()
-   inv <- solve(data)
-   x$setinverse(inv)
-   inv
+
+cacheSolve <- function(x,...)
+{
+ 		if(!is.null(lee))
+ 		{
+         	message("Getting cached Inverse")
+         	}
+         else
+	         {	
+         	message("Creating new Inverse")
+         	lee <<- solve(bob)
+         	}
+	lee
 }
+
